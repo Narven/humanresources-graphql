bfba06e9d60ed6a7d66c71a8e4c1597bc668d2e0
Pedro Luz <pedromsluz@gmail.com>
2021-01-08 20:36:36 +0000

    feat: configure user schema in user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserResolver } from './user.resolver';
+import { UserSchema } from './user.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  providers: [UserResolver],
+})
 export class UserModule {}

5fdd7bc9c6a6f36f41a552b3d94aec02432246df
Pedro Luz <pedromsluz@gmail.com>
2021-01-08 20:57:38 +0000

    feat: and the missing service

---
+++ src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User, UserDocument } from './user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createUser = new this.userModel(createUserDto);
+    return await createUser.save();
+  }
+}

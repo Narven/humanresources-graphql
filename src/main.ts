adc421b3ba6cf54672b9967641b002664583b9f9
Pedro Luz <pedromsluz@gmail.com>
2021-01-09 00:16:57 +0000

    feat: 🔃️ remove

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
@@ -7,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
-  app.use(csurf());
   await app.listen(3000);
 }
 

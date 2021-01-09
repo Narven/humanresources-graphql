c502688f1cb057bc86b735e2c0945877f3916a92
Pedro Luz <pedromsluz@gmail.com>
2021-01-09 00:14:48 +0000

    feat: 🔃️ setup cross-site request forgery (CSRF)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
+  app.use(csurf());
   await app.listen(3000);
 }
 
